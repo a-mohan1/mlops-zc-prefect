2018320eb2506ae53f8408e297215a0a2c7b4f8a
A M <aruna.mohan1@gmail.com>
2025-12-18 15:30:35 -0500

    3.6 updates

--- 3.5/create_s3_bucket_block.py
+++ 3.5/create_s3_bucket_block.py
@@ -18,7 +18,7 @@ def create_s3_bucket_block():
     my_s3_bucket_obj = S3Bucket(
         bucket_name="zoomcamp-bucket", credentials=aws_creds
     )
-    my_s3_bucket_obj.save(name="s3-bucket", overwrite=True)
+    my_s3_bucket_obj.save(name="s3-bucket-block", overwrite=True)
 
 
 if __name__ == "__main__":
